f80dbf584e50ae9d013e351e6758242d382107e6
Ashley Narcisse-Austin <Ashley_NarcisseAustin@ultimatesoftware.com>
2016-12-04 20:06:43 -0500

    added integration for rescuetime

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const api = express.Router();
 const bot = require('./bot');
 const messages = require('./data/messages');
+const rescue = require('./integrations/rescuetime');
 
 api.route('/bot')
 	.get(bot)
 	.post(bot);
 
 api.get('/messages', (req, res) => res.json(messages));
-
+api.get('/integration/rescuetime', rescue);
 
 module.exports = api;
